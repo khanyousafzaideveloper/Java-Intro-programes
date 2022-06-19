492feba0035d7662eb54aa78146b37181593a9c9
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-19 20:27:11 +0500

    variables

---
+++ variables.java
@@ -0,0 +1,38 @@
+import java.lang.invoke.ConstantBootstraps;
+
+//type variableName = value;
+public class variables {
+    public static void main(String[] args) {
+
+        // declaring int variable
+        int age = 30;
+        System.out.println(age);
+
+// declaring String variable
+
+      String name = "Yousafzai";
+      System.out.println(name); 
+
+      // declaring double varibale
+      
+        double some_float = 5.32;
+      System.out.println(some_float);
+
+      // declaring char variable
+
+      char grade = 'A';
+      System.out.print(grade);
+
+      // declaring boolean variable
+      
+      boolean is_raining = true;
+     System.out.println(is_raining);
+
+     /////////////////////////////////////////////////////////////////////
+    
+     int mynum = 23;
+     mynum = 98;
+     System.out.println(mynum);
+
+    }
+  }
