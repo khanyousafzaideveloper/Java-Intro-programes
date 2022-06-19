d787877f9ffa8332cc55750bb25af375a643edfa
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-19 20:59:43 +0500

    type_casting

---
+++ Java_type_casting.java
@@ -0,0 +1,11 @@
+public class Java_type_casting{
+    public static void main(String[] argv){
+
+        int myint = 6;
+        double mydouble = myint;  // type casting: int to double
+
+        System.out.println(myint);  //prints int
+        System.out.println(mydouble);   // prints double
+
+    }
+}
