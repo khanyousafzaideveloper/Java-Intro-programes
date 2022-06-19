7affcf7389cdac493c138a2af6001fa83806939a
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-19 20:49:12 +0500

    moreJava

---
+++ scientific_numbers.java
@@ -0,0 +1,13 @@
+public class scientific_numbers {
+    public static void main(String[] args){
+        
+        float myNum = 5.75f;
+        System.out.println(myNum);
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+        
+    }
+}
