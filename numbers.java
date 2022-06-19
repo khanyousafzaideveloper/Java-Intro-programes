118063ff45dd3c3540aa76146c1ad6c00f6cf386
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-19 09:24:15 +0500

    numbers

---
+++ numbers.java
@@ -0,0 +1,14 @@
+public class numbers {
+    public static void main(String[] args)
+    {
+        System.out.println(4+2);
+        System.out.println(24-2);
+        System.out.println(34*2);
+        System.out.println(34/2);
+        System.out.println(54%5);
+        System.out.println(89%3);
+        System.out.println((23*21)/(2*3));
+        System.out.println((3*2*1*6) / (3+5));
+
+    }
+}
