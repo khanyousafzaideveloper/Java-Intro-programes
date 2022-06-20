abb41f2e80106feaebcc3605ede5d64912a3c042
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-20 14:54:44 +0500

    for each

---
+++ for_each_loop.java
@@ -0,0 +1,10 @@
+public class for_each_loop {
+    public static void main(String[] argv)
+    {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        for (String i : days){
+            System.out.println(i);
+        }
+    }
+    
+}
