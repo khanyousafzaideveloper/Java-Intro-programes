da3c2e3270df4437c08c219e10a3333ec8f337b3
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-20 14:48:33 +0500

    loops

---
+++ loops.java
@@ -0,0 +1,31 @@
+public class loops {
+
+    public static void main(String[] argv)
+    {
+        int var=0;
+        while(var < 5)
+        {
+            System.out.println(var);
+            var++;
+        }
+
+        // do while loop
+        System.out.println("__________________________________________________________________________");
+
+        int i = 0;
+        do {
+        System.out.println(i);
+        i++;
+        }
+        while (i < 5);
+
+
+        System.out.println("___________________________________________________________________________");
+
+        for(int j= 0; j < 10 ; j++)
+        {
+            System.out.println(j);
+        }
+    }
+    
+}
