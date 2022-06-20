56b5791913dacde25f400abe1fd62173d0d01b2b
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-20 10:12:10 +0500

    ternanry

---
+++ ternary.java
@@ -0,0 +1,9 @@
+public class ternary {
+    public static void main(String[] args)
+    {
+        int age = 15;
+        String eligible = (age>=18) ? "You are Eligible " : "Not Elligible";
+        System.out.println(eligible);
+    }
+    
+}
