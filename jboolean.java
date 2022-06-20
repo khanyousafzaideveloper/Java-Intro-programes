d2f09915df249ea9df530b9bc0bea6306abfe7af
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-20 08:04:49 +0500

    boolean

---
+++ jboolean.java
@@ -0,0 +1,20 @@
+public class jboolean{
+    public static void main(String[] argv)
+    {
+        boolean JavaIsFun = true;
+        boolean isRaining = false;
+
+        System.out.println(JavaIsFun);
+        System.out.println(isRaining);
+
+        // boolean expressions
+        
+        int x = 3, y= 2;
+
+        System.out.println(x>y);
+        System.out.println(x==y);
+        
+        
+    }
+    
+}
