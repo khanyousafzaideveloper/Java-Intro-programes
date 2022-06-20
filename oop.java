b0463dd94280aae3f476c8de86c9e995d8add09f
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-21 04:49:14 +0500

    oop_two_classes_seprate_files

--- oop.java
+++ oop.java
@@ -1,9 +1,11 @@
 public class oop {
-    int x= 3;
+    int x=4;
     public static void main(String[] args)
     {
         oop obj1 = new oop();
+        oop obj2 = new oop();
         System.out.println(obj1.x);
+        System.out.println(obj2.x);
     }
     
 }
---
+++ oop_two_classes_in_Java/second.java
@@ -0,0 +1,8 @@
+public class second {
+    public static void main(String[] argv)
+    {
+        Main obj1 = new Main();
+        System.out.println(obj1.x);
+    }
+    
+}
