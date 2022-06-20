edd1b030cd7b8c46187a7631afd92c2ca1643021
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-20 19:30:24 +0500

    methods

---
+++ methods.java
@@ -0,0 +1,12 @@
+public class methods {
+  
+    static void myMethod()
+    {
+        System.out.println("I am trying to learn Java methods");
+    }
+
+    public static void main(String[] argv){ 
+    
+    myMethod();
+}
+    }
