648a98af92c5863ee697d663ad17f8cd54f097df
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-20 20:53:57 +0500

    parameters to methods

--- methods.java
+++ methods.java
@@ -5,10 +5,22 @@ public class methods {
         System.out.println("I am trying to learn Java methods.");
     }
 
+    static void first_name(String fname)
+    {
+        System.out.println(fname + " " + "khan");
+    }
+
+
     public static void main(String[] argv){ 
     
     myMethod();
     myMethod();
     myMethod();
-}
+    first_name("Yousaf");
+    first_name("Ali");
+    first_name("Ahmad");
+
     }
+
+    
+}
