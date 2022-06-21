2150dc68b4d3af2cb0be4edc8489755131208fa8
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-21 05:01:40 +0500

    modify_att

---
+++ JAVA_OOP/modify_att.java
@@ -0,0 +1,10 @@
+public class modify_att {
+ int x ;
+ public static void main(String[] argv)
+ {
+    modify_att obj1 = new modify_att();
+    obj1.x = 23;
+    System.out.println(obj1.x);
+ }
+ 
+}
