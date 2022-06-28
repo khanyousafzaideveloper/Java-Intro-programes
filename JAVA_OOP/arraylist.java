7ad44b42b7a9233f5f1dbafb0d2190135072543a
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-28 09:19:55 +0500

    array

---
+++ JAVA_OOP/arraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;;
+
+public class arraylist {
+    public static void main(String[] args)
+    {
+        ArrayList<String>  cars = new ArrayList<String>();
+        cars.add("Toyota");
+        cars.add("Suzuki");
+        cars.add("Porche");
+        cars.add("Ford");
+
+        System.out.println(cars);
+    }
+}
