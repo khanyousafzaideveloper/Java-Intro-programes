67550c101a4d0e62b67b783b180b5b727becfe06
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-28 23:19:24 +0500

    hashSets

---
+++ JAVA_OOP/hashsets.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+ 
+// A HashSet is a collection of items where every item is unique.
+ 
+public class hashsets {
+    public static void main(String[] args)
+    {
+        HashSet<String> cars = new HashSet<String>();
+        cars.add("Toyota");
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Toyota");
+        cars.add("Ford");
+
+        System.out.println(cars);
+        
+        // To check whether an item exists in a HashSet, use the contains() method:
+
+        System.out.println(cars.contains("Volvo"));
+
+        //To remove an item, use the remove() method:
+
+        cars.remove("BMW");
+
+        //To remove all items, use the clear() method:
+
+        cars.clear();
+
+        System.out.println(cars);
+    }
+    
+}
