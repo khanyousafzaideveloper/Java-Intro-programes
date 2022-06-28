1a6b24f4ec53ef7f8d0d7c887d4b65488618c9f9
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-28 09:31:21 +0500

    array__

--- JAVA_OOP/arraylist.java
+++ JAVA_OOP/arraylist.java
@@ -10,5 +10,35 @@ public class arraylist {
         cars.add("Ford");
 
         System.out.println(cars);
-    }
+
+         // accessing items
+        System.out.println(cars.get(1));
+        System.out.println(cars.get(0));
+
+ System.out.println("****************************************************");
+
+        //Changing an item in array
+        cars.set(2, "ferrari");
+
+        //Removing from array
+        cars.remove(2);
+
+        //size of array
+        System.out.println(cars.size());
+System.out.println("****************************************************");
+
+        // loop through array list
+        for(int i=0; i < cars.size(); i++)
+        {
+            System.out.println(cars.get(i));
+        }
+
+System.out.println("****************************************************");
+
+        //for each loop
+        for(String x : cars)
+        System.out.println(x);
+
+}
+      
 }
