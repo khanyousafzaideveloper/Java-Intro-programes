bed6c7f071f18e2930a2bc6926abca47ac75d55d
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-24 11:57:09 +0500

    BSStudent

---
+++ java_in_course/Student_detail/.settings/BSStudent.java
@@ -0,0 +1,26 @@
+package .settings;
+
+public class BSStudent extends Student{
+
+    int semester;
+    double cgpa;
+
+    void set_semester(int s)
+    {
+        semester = s;
+    }
+    void set_cgpa(double c)
+    {
+        cgpa = c;
+    }
+    int get_semester()
+    {
+        return semester;
+    }
+    double get_cgpa()
+    {
+        return cgpa;
+    }
+    
+}
+
