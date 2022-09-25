77adf2ab201d8479ecd6f645c96f02582340ec42
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-25 09:58:29 +0500

    classes _ inheritence

--- java_in_course/Student_detail/Testclass.java
+++ java_in_course/Student_detail/Testclass.java
@@ -1,13 +1,14 @@
-public class Testclass extends Student {
+public class Testclass{
     public static void main(String[] args)
     {
-        Testclass t1 = new Testclass();
+        Student t1 = new Student();
+        BSStudent b1 = new BSStudent();
         t1.set_name("Tom");
         t1.set_roll(23);
-        t1.set_semester(3);
+        b1.set_semester(3);
         System.out.println(t1.get_name());
         System.out.println(t1.get_roll());
-        System.out.println(t1.get_semester());
+        System.out.println(b1.get_semester());
         System.out.println(t1.get_name());
     }
     
