5177a46c57d808670fb8d009700ffd037784054b
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-25 14:22:32 +0500

    ..

--- java_in_course/Student_detail/Student.java
+++ java_in_course/Student_detail/Student.java
@@ -3,25 +3,18 @@ public class Student {
 		int roll;
 		String name;
 		
-		void set_name(String n) {
+		public void set_name(String n) {
 			name = n;
 		}
-		void set_roll(int r) {
+		public void set_roll(int r) {
 			roll = r;
 		}
 		public String get_name()
 		{
 			return name;
 		}
-		int get_roll()
+		public int get_roll()
 		{
 			return roll;
-		}
-		public static void main(String[] args)
-		{
-			Student s = new Student();
-			s.set_name("Ajmal");
-			s.get_name();
-			
 		}
 }
