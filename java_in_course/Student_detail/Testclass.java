1b43193f05dcad583b15dd8fb0eec369119a338d
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-26 19:22:26 +0500

    .

--- java_in_course/Student_detail/Testclass.java
+++ java_in_course/Student_detail/Testclass.java
@@ -3,10 +3,12 @@ public class Testclass{
     {
         
         Student b1 = new Student();
-        b1.set_roll(34);;
+        b1.set_roll(34);
+        b1.set_name("Anonymous");
+        b1.set
 
         System.out.println(b1.get_roll());
-        //System.out.println(b1.get_name());
+        System.out.println(b1.get_name());
     }
     
 }
