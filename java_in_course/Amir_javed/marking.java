c413de3a9eac4abba24c90b7c9db38718fbca412
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-10-02 08:37:52 +0500

    grades are printed

--- java_in_course/Amir_javed/marking.java
+++ java_in_course/Amir_javed/marking.java
@@ -7,7 +7,25 @@ class marking {
         System.out.println("Enter grade of Student: A, B, C, D and F");
         char grade = sc.next().charAt(0);
         grade = Character.toUpperCase(grade);
+        System.out.println("+, - to change value and X for not changing it: ");
+        char sign = sc.next().charAt(0);
+        
         System.out.println(grade);
-    
+        if(grade=='A')
+        {
+            System.out.println(4);
+        }
+        else if(grade =='B'){
+            System.out.println(3);
+        }
+        else if(grade =='C'){
+            System.out.println(2);
+        }
+        else if(grade =='D'){
+            System.out.println(1);
+        }
+        else{
+            System.out.println('0');
+        }
 }  
 }
