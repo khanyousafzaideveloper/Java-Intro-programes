c3672307e4b6f2b955f66a8c24fd18986a0d3b51
Ahmad Khan Yousafzai <kahnyousafzaideveloper@gmail.com>
2022-10-04 22:56:54 +0500

    class Student_array

--- java_in_course/Sorting_algo/Student_array.java
+++ java_in_course/Sorting_algo/Student_array.java
@@ -1,8 +1,18 @@
 public class Student_array {
-    public static void main()
+    private String name;
+    private int age;
+    Student_array(String n, int a)
     {
-        Student s1 = new Student();
-        s1.set_name("Ahmad");
-        s1.get_name();
+        name = n;
+        age = a;
+    }
+    public void get_info()
+    {
+        System.out.println("Name: " + name+",  Age: "+age );
+    }
+    public static void main(String[] args)
+    {
+        Student_array s1 = new Student_array("Amjad", 32);
+        s1.get_info();
     }
 }
