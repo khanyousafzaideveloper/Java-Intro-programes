bd8cabfacaa374395f2f303ba879cfec2ee9eb76
Ahmad Khan Yousafzai <kahnyousafzaideveloper@gmail.com>
2022-10-05 17:29:08 +0500

    checking

--- java_in_course/Student_detail/Testclass.java
+++ java_in_course/Student_detail/Testclass.java
@@ -5,7 +5,7 @@ public class Testclass{
         Student b1 = new Student();
         b1.set_roll(34);
         b1.set_name("Anonymous");
-        b1.set
+        b1.name("hello");
 
         System.out.println(b1.get_roll());
         System.out.println(b1.get_name());
