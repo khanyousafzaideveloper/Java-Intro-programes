b05a398b4c0c5989d20ce115c7d9003bd6017e59
Ahmad Khan Yousafzai <kahnyousafzaideveloper@gmail.com>
2022-10-06 20:26:45 +0500

    parent and child class

---
+++ java_in_course/inheritence_in_vscode/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    private int age;
+    private String group;
+
+    Animal(int age, String group){
+        this.age = age;
+        this.group = group;
+    }
+    public void get_info()
+    {
+        System.out.println("Age: "+age);
+        System.out.println("group: "+group);
+    }
+}
