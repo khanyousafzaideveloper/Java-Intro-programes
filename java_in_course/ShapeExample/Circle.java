e2a6077b456df9ae25ba633c89e331b82a59bde1
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2022-10-12 17:05:54 +0500

    circle implemented

--- java_in_course/ShapeExample/Circle.java
+++ java_in_course/ShapeExample/Circle.java
@@ -1,3 +1,15 @@
-public class Circle {
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public double get_area()
+    {   
+        return Math.PI*radius*radius;
+    }
+    public double get_parameter()
+    {
+        return 2*Math.PI*radius;
+    }
     
 }
