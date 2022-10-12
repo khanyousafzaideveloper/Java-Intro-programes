c0ca6bffc2006830d4bcdff8a92f1b7e065498f2
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2022-10-12 17:01:10 +0500

    Square class implemented

---
+++ java_in_course/ShapeExample/Shape.java
@@ -0,0 +1,5 @@
+abstract class Shape {
+    abstract public double get_area();
+    abstract public double get_parameter();
+    
+}
--- java_in_course/ShapeExample/Square.java
+++ java_in_course/ShapeExample/Square.java
@@ -1,3 +1,19 @@
-public class Square {
+public class Square extends Shape{
+
+    private double length;
+
+    public Square(double l)
+    {
+        length = l;
+    }
+    @Override
+    public double get_area() {
+        return length*length;
+    }
+
+    @Override
+    public double get_parameter() {
+        return length*4;
+    }
     
 }
