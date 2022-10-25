791f1038d4873c74d65b5d3f285a1751b1e4d383
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2022-10-25 18:00:07 +0500

    ...

---
+++ java_in_course/threadsCounter/IncrementingThread.java
@@ -0,0 +1,11 @@
+public class IncrementingThread extends Thread{
+    Counter c;
+    public IncrementingThread(Counter c)
+    {
+        this.c = c;
+    }
+    public void run()
+    {
+        c.increment();
+    }
+}
