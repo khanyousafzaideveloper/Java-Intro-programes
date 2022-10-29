d9f15713e108bb9055ec36d0c9a3b3d4ec078dda
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2022-10-29 11:54:12 +0500

    ....

--- java_in_course/ThreadsExample2/Mythread.java
+++ java_in_course/ThreadsExample2/Mythread.java
@@ -1,15 +1,21 @@
 public class Mythread extends Thread {
+    
     public static void main(String[] args){
      
         Mythread t1 = new Mythread();
         Mythread t2 = new Mythread();
         Mythread t3 = new Mythread();
+
         t1.start();
+        System.out.println(t1.getId());
+        t1.setPriority(9);
         t2.start();
         t3.start();        
 
     }
     public void run() {
-        System.out.println("Some code with a thread: " + getId());
+        for(int i = 0; i<10; i++){ 
+            System.out.println(i+ ": Some code with a thread: " + currentThread().getName());
+        }
     }
 }
