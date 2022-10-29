ef4995a40a43ccbf2180d7c8292b57f39a537eb5
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2022-10-29 10:18:51 +0500

    thread_id

--- java_in_course/ThreadsExample2/Mythread.java
+++ java_in_course/ThreadsExample2/Mythread.java
@@ -2,9 +2,14 @@ public class Mythread extends Thread {
     public static void main(String[] args){
      
         Mythread t1 = new Mythread();
+        Mythread t2 = new Mythread();
+        Mythread t3 = new Mythread();
         t1.start();
+        t2.start();
+        t3.start();        
+
     }
     public void run() {
-        System.out.println("Some code with a thread");
+        System.out.println("Some code with a thread: " + getId());
     }
 }
