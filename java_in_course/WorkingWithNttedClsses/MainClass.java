5d99612db7255b50f46f008a7e5e039e9a304a5b
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2022-11-05 09:01:09 +0500

    anonymous class

--- java_in_course/WorkingWithNttedClsses/MainClass.java
+++ java_in_course/WorkingWithNttedClsses/MainClass.java
@@ -1,14 +1,18 @@
+//Outer class
 public class MainClass {
     int x = 23;
-    public int  add(int x, int y)
-    {
+    public int  add(int x, int y){
         return x+y;
-
     }
+    public void show(){
+        System.out.println("This is Outer class");
+    }
+    // inner class
     class inner{ 
         int y = 21;
         public int multiply(int x, int y){
             return x*y;
         }
     }
+
 }
--- java_in_course/WorkingWithNttedClsses/TestClassx.java
+++ java_in_course/WorkingWithNttedClsses/TestClassx.java
@@ -1,11 +1,25 @@
 public class TestClassx{
     public static void main(String[] args)
-    {
+    {   
+        //for Outerclass
         MainClass m1 = new MainClass();
         System.out.println(m1.add(3,5));
-
+        
+        //for inner class
         MainClass.inner myinner = m1.new inner();
         System.out.println(myinner.multiply(2, 8));
+
+
+        System.out.println(m1.add(2, 3) + myinner.multiply(2, 4));
+
+        //for anonymous inner class
+        MainClass anon  = new MainClass(){
+            public void show()
+            {
+                System.out.println("This is anonymous inner Class");
+            }
+        }; 
         
+        anon.show();
     }    
 }
